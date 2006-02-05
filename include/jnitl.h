de1b33a93915b5476127a149b67175b260949d17
kohsuke_agent <null>
2006-02-05 17:46:36 +0000

    added linker statement for VC++

--- include/jnitl.h
+++ include/jnitl.h
@@ -1,6 +1,12 @@
 // JNI template library
 #pragma once
 
+#ifdef _DEBUG
+	#pragma comment(lib, "jnitld.lib")
+#else
+	#pragma comment(lib, "jnitl.lib")
+#endif
+
 #include "jnitl_module.h"
 #include "jnitl_array.h"
 #include "jnitl_accessor.h"
